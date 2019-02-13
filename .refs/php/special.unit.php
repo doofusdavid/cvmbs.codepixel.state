168f1dc8aa38f48b0384287388dc7963a00b69ff
codepixelstudio <codepixel@codepixelstudio.com>
2019-02-13 16:35:34 -0700

    initiate SPECIAL UNIT site template development
    misc. updates to SCSS files and initial PHP templates

---
+++ .refs/php/special.unit.php
@@ -0,0 +1,30 @@
+<!-- billboard.layers -->
+<div id="billboard-artwork-layers">
+
+    <!-- headline -->
+    <span class="headline">
+
+        make a difference
+
+    </span>
+    <!-- END headline -->
+
+    <!-- text -->
+    <span class="description">
+
+        more than a century of excellence in teaching advanced<br />
+        veterinary medicine and compassionate care
+
+    </span>
+    <!-- END text -->
+
+    <!-- button -->
+    <button class="explore-button menu-item-link" data-section-link="overview">
+
+        explore our program
+
+    </button>
+    <!-- END button -->
+
+</div>
+<!-- END billboard.layers -->
