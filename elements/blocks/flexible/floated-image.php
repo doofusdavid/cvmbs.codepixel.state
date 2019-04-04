5ad707888aff947ee42c48fa105f992bc6e03b47
Tim Blodgett <mr.tim.blodgett@gmail.com>
2019-04-04 13:58:25 -0600

    remove Block Width option from Flexible page blocks; rename Announcement block to Notification block; fix issue preventing Floated Image position for image to be set

--- elements/blocks/flexible/floated-image.php
+++ elements/blocks/flexible/floated-image.php
@@ -1,11 +1,12 @@
 <?php
 $img = get_sub_field('img');
 $img_width = $img['width'] . 'px';
+$img_pos = get_sub_field('img_pos');
 ?>
 
 <div class="template-block">
 	<div class="template-block__inner floated-image">
-		<figure class="floated-image--left" width="<?php echo $img['width']; ?>">
+		<figure class="floated-image--<?php echo esc_attr( $img_pos ); ?>" width="<?php echo $img['width']; ?>">
 
 			<?php echo wp_get_attachment_image( $img['ID'], 'large' ); ?>
 
