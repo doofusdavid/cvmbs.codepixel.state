0116dab69aab6566691877da1063fb23a6612b96
Tim Blodgett <mr.tim.blodgett@gmail.com>
2019-04-10 11:40:15 -0600

    update Degree Program block styling; remove Text Editor block from Degree Program template

--- elements/blocks/programs/financial.php
+++ elements/blocks/programs/financial.php
@@ -12,13 +12,26 @@ if ( have_rows('tuition_financial_aid') ) :
 
 		<h2 class="title"><?php the_sub_field('heading'); ?></h2>
 
-		<span class="content">
-			<?php // the_sub_field('desc'); ?>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non dapibus lacus. Aenean sodales, mauris nec ornare eleifend, eros arcu molestie felis, id ornare est erat sed velit. Quisque hendrerit vitae justo a tristique. Nullam purus ipsum, venenatis ultricies dictum ac, lacinia eget dolor. Curabitur et rhoncus augue. Nullam aliquet tortor non ultricies blandit. Proin nec libero at sapien lobortis gravida eu non ipsum. Suspendisse pellentesque dapibus gravida. Sed faucibus elit in dignissim malesuada.</p>
-		</span>
+		<div class="content">
+
+			<?php if ( get_sub_field('desc') ) : ?>
+
+				<?php the_sub_field('desc'); ?>
+
+			<?php else : ?>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non dapibus lacus. Aenean sodales, mauris nec ornare eleifend, eros arcu molestie felis, id ornare est erat sed velit. Quisque hendrerit vitae justo a tristique. Nullam purus ipsum, venenatis ultricies dictum ac, lacinia eget dolor. Curabitur et rhoncus augue. Nullam aliquet tortor non ultricies blandit. Proin nec libero at sapien lobortis gravida eu non ipsum. Suspendisse pellentesque dapibus gravida. Sed faucibus elit in dignissim malesuada.</p>
+
+			<?php endif; ?>
+
+		</div>
 
 		<!-- button -->
-		<button class="content-button" data-link="<?php // echo esc_url( $cta['url'] ); ?>">FIX: <?php // echo esc_attr( $cta['title'] ); ?> Button Text Goes Here</button>
+		<?php if ( $cta ) : ?>
+			<a class="content-button" href="<?php echo esc_url( $cta['url'] ); ?>">
+				<?php echo esc_attr( $cta['title'] ); ?>
+			</a>
+		<?php endif; ?>
 		<!-- END button -->
 
 	</div>
--- elements/blocks/programs/visit-foco.php
+++ elements/blocks/programs/visit-foco.php
@@ -12,19 +12,32 @@ if ( have_rows( 'visit_foco', 'option' ) ) :
 
 		<h3 class="title"><?php the_sub_field('heading'); ?></h3>
 
-		<span class="content">
+		<div class="content">
 
-			<?php the_sub_field('desc'); ?>
+			<?php if ( get_sub_field('desc') ) : ?>
 
-		</span>
+				<?php the_sub_field('desc'); ?>
 
-		<!-- button -->
-		<button class="content-button">
+			<?php else: ?>
 
-			FIX: schedule a tour of campus
+				<p>Fort Collins blends education and industry, hard work and outdoor recreation, tradition and progress. If you’re looking for a college experience with the perfect fusion of culture, opportunity and adventure, Fort Collins is where you’ll find it. Located at the foothills of the Rocky Mountains, Fort Collins provides extensive access to hundreds of miles of trails for biking and hiking, world-class fishing and rock climbing, and camping, all in an area with over 300 days of sun per year.</p>
 
-		</button>
-		<!-- END button -->
+			<?php endif; ?>
+
+		</div>
+
+		<?php
+		if ( have_rows('ctas') ) :
+			while ( have_rows('ctas') ) : the_row();
+				$link = get_sub_field('link');
+		?>
+			<!-- button -->
+			<a class="content-button" href="<?php echo esc_url( $link['url'] ); ?>"><?php echo esc_attr( $link['title'] ); ?></a>
+			<!-- END button -->
+		<?php
+			endwhile;
+		endif;
+		?>
 
 	</div>
 	<!-- END content wrapper -->
