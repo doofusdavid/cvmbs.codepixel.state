d01a2d1191a84e8ec99f3159cb821193fadcd7e0
codepixelstudio <codepixel@codepixelstudio.com>
2019-04-24 09:58:34 -0600

    directory app updates

--- pages/directory.php
+++ pages/directory.php
@@ -35,7 +35,14 @@
     );
 
     // store in normalized array
-    $data = json_encode( $directory, JSON_PRETTY_PRINT );
+    // $data = json_encode( $directory, JSON_PRETTY_PRINT );
+    $data = json_encode( $directory );
+
+    // create json store
+    $storage = $_SERVER[ 'DOCUMENT_ROOT' ] . '/wp-content/themes/cvmbsPress/data/directory.json';
+
+    // send data to json store
+    file_put_contents( $storage, $data );
 
     // revise array data
     $data = $directory->GetMembersBySearchNameResult->MemberResponse;
@@ -43,99 +50,217 @@
 ?>
 
 <!-- directory -->
-<main id="directory" class="off-canvas-content <?php echo $site_type; ?>" data-off-canvas-content>
+<main id="site-layout" class="off-canvas-content <?php echo $site_type; ?>" data-off-canvas-content>
 
-    <!-- toolbar.DEV -->
-    <div id="directory-toolbar" class="toolbar">
+    <!-- directory -->
+    <div id="directory" class="page-container">
 
-        <span id="query">
+        <!-- page header -->
+        <header class="header">
 
-            <span class="query-label">
+            <h1>
 
-                you searched for&nbsp;
+                <?php the_title(); ?>
 
-            </span>
+            </h1>
 
-            <span class="query-string">
+        </header>
+        <!-- END page header -->
 
-                <?php echo $query; ?>
+        <!-- toolbar.DEV -->
+        <div id="directory-toolbar" class="toolbar">
 
-            </span>
+            <!-- alphabet -->
+            <div id="directory-alphabet" class="toolbar-control-group">
 
-        </span>
 
-        <span id="title">
 
-            directory<span>.</span><em>app</em><span>[</span>&nbsp;<strong>prototype</strong>&nbsp;<span>]</span>
+            </div>
+            <!-- END alphabet -->
 
-        </span>
+            <!-- fiels -->
+            <div id="directory-fields" class="toolbar-control-group">
 
-    </div>
-    <!-- END toolbar.DEV -->
 
-    <!-- table -->
-    <table id="directory-records">
 
-        <thead>
+            </div>
+            <!-- END fiels -->
 
-            <tr>
+        </div>
+        <!-- END toolbar.DEV -->
 
-                <th>
+        <!-- table -->
+        <table id="directory-records">
 
-                    Name
+            <thead>
 
-                </th>
+                <tr>
 
-                <th>
+                    <th>
 
-                    E-mail Address
+                        Name
 
-                </th>
+                    </th>
 
-                <th>
+                    <th>
 
-                    Phone
+                        E-mail Address
 
-                </th>
+                    </th>
 
-            </tr>
+                    <th>
 
-        </thead>
+                        Phone
 
-        <tbody>
+                    </th>
+
+                </tr>
+
+            </thead>
+
+            <tbody>
+
+                <?php
+
+                    foreach ( $data as $person ) {
+
+                        $ename     = $person->EName;
+                        $lastName  = $person->LastName;
+                        $firstName = $person->FirstName;
+                        $eMail     = strtolower( $person->EmailAddress );
+                        $phone     = $person->MemberContacts->MemberContactResponse->PhoneNumber;
+
+                        $results .= '<tr class="record"><td>' . $firstName . ' ' . $lastName . '</td><td><a class="email-link" href="mailto:' . $eMail . '">' . $eMail . '</a></td><td>' . $phone . '</td></tr>';
+
+                    }
+
+                    echo $results;
+
+                ?>
+
+            </tbody>
+
+        </table>
+        <!-- END table -->
+
+        <!-- pagination -->
+        <div id="directory-controls" class="toolbar">
+
+
+
+        </div>
+        <!-- END pagination -->
+
+        <!-- info -->
+        <div id="directory-info" class="toolbar">
+
+
+
+        </div>
+        <!-- END info -->
+
+        <pre>
 
             <?php
 
-                foreach ( $data as $person ) {
-
-                    $ename     = $person->EName;
-                    $lastName  = $person->LastName;
-                    $firstName = $person->FirstName;
-                    $eMail     = $person->EmailAddress;
-                    $phone     = $person->MemberContacts->MemberContactResponse->PhoneNumber;
-
-                    $results .= '<tr class="record"><td>' . $firstName . ' ' . $lastName . '</td><td><a class="email-link" href="mailto:' . $eMail . '">' . $eMail . '</a></td><td>' . $phone . '</td></tr>';
-
-                }
-
-                echo $results;
+                // print_r( $data );
 
             ?>
 
-        </tbody>
+        </pre>
 
-    </table>
-    <!-- END table -->
+        <!-- Data Tables -->
+        <script type="text/javascript" src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript">
 
-    <pre>
+            // alphabet search
+            var _alphabetSearch = '';
 
-        <?php
+            // push search settings to app
+            $.fn.dataTable.ext.search.push( function ( settings, searchData ) {
 
-            print_r( $data );
+                if ( ! _alphabetSearch ) {
 
-        ?>
+                    return true;
 
-    </pre>
+                }
+
+                if ( searchData[0].charAt(0) === _alphabetSearch ) {
+
+                    return true;
+
+                }
+
+                return false;
+
+            });
+
+            // ready
+            $(document).ready(function(){
+
+                // initialization
+                var controlphabet = $('#directory-alphabet');
+                var controlfields = $('#directory-fields');
+                var directoryinfo = $('#directory-info');
+                var controlpages  = $('#directory-controls');
+                var table   = $('#directory-records').DataTable( {
+
+                    'order'   : [[ 0, 'asc' ]],
+                    stateSave : true
+
+                });
+
+                // alphabet search
+                var alphabet = $('<div class="alphabet"/>').append( '<span class="alphabet-label">Last Name: </span>' );
+
+                // add clear action to alphabet
+                $('<span class="alphabet-control clear active"/>')
+                    .data( 'letter', '' )
+                    .html( 'None' )
+                    .appendTo( alphabet );
+
+                // increment
+                for ( var i = 0 ; i < 26 ; i++ ) {
+
+                    var letter = String.fromCharCode( 65 + i );
+
+                    $('<span class="alphabet-control letter"/>')
+                        .data( 'letter', letter )
+                        .html( letter )
+                        .appendTo( alphabet );
+
+                }
+
+                // prepend to page content container
+                // alphabet.insertBefore( table.table().container() );
+                alphabet.appendTo( controlphabet );
+
+                $('#directory-records_length').appendTo( controlfields );
+                $('#directory-records_filter').appendTo( controlfields );
+                $('#directory-records_info').appendTo( directoryinfo );
+                $('#directory-records_paginate').appendTo( controlpages );
+
+                // toggle classes
+                alphabet.on( 'click', 'span', function () {
+
+                    alphabet.find( '.active' ).removeClass( 'active' );
+
+                    $(this).addClass( 'active' );
+
+                    _alphabetSearch = $(this).data('letter');
+
+                    table.draw();
+
+                } );
+
+            });
+
+        </script>
+
+    </div>
+    <!-- END directory -->
+
+    <?php get_template_part( 'elements/layout/layout.footer' ); ?>
 
 </main>
 <!-- END directory -->
