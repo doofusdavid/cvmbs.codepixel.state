f83a488f0b8e644b62e8d35a09796e17d820bd79
Tim Blodgett <mr.tim.blodgett@gmail.com>
2019-05-10 10:59:58 -0600

    add optional Description area to Program Contacts block on Degree Program template

--- elements/blocks/programs/contacts.php
+++ elements/blocks/programs/contacts.php
@@ -1,8 +1,16 @@
 <div class="program-contact program-block">
 	<div class="program-contact__inner">
-		<h2 class="program-contact__title program-block__title">
-			<?php _e( 'Contact Us', 'cvmbsPress'); ?>
-		</h2>
+		<div class="program-contact__intro">
+			<h2 class="program-contact__title program-block__title">
+				<?php _e( 'Contact Us', 'cvmbsPress'); ?>
+			</h2>
+
+			<?php if ( get_sub_field('desc') ) : ?>
+			<div class="program-contact__desc">
+				<?php the_sub_field('desc'); ?>
+			</div><!-- .program-contact__desc -->
+			<?php endif; ?>
+		</div><!-- .program-contact__intro -->
 
 		<div class="program-contact__content">
 			<?php the_sub_field('form'); ?>
