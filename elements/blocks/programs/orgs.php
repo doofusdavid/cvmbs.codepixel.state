399ab8b10ee573549816f5b6c0e0b7ddd4c1b113
Tim Blodgett <mr.tim.blodgett@gmail.com>
2019-05-24 15:01:19 -0600

    fix issue: Student Organizations not linking to external sites on Degree Program pages

--- elements/blocks/programs/orgs.php
+++ elements/blocks/programs/orgs.php
@@ -10,9 +10,12 @@
 		if ( have_rows( 'orgs' ) ) :
 			while ( have_rows( 'orgs' ) ) : the_row();
 				$org = get_sub_field('org');
+				$link = ( get_field( 'student_org_link', $org ) ) ? get_field( 'student_org_link', $org ) : get_permalink( $org );
 		?>
 
-			<a class="organizations__button" href="<?php echo esc_url( get_permalink( $org ) ); ?>"><?php echo esc_attr( get_the_title( $org ) ); ?></a>
+			<a class="organizations__button" href="<?php echo esc_url( $link ); ?>">
+				<?php echo esc_attr( get_the_title( $org ) ); ?>
+			</a>
 
 		<?php
 			endwhile;
