ac3a7422486cf2162a4f8b4d6f3410b29e4bf8e5
Tim Blodgett <mr.tim.blodgett@gmail.com>
2019-06-04 10:08:57 -0600

    add Visit Fort Collins block back to single Degree Program pages

--- elements/blocks/programs/visit-foco.php
+++ elements/blocks/programs/visit-foco.php
@@ -1,7 +1,4 @@
-<?php
-if ( have_rows( 'visit_foco', 'option' ) ) :
-	while ( have_rows( 'visit_foco', 'option' ) ) : the_row();
-?>
+<?php $visit = get_field( 'visit_foco', 'options' ); ?>
 
 <!-- location -->
 <div class="location program-block">
@@ -10,24 +7,24 @@ if ( have_rows( 'visit_foco', 'option' ) ) :
 
 		<div class="location__content">
 
-			<h2 class="location__title program-block__title"><?php the_sub_field('heading'); ?></h2>
+			<h2 class="location__title program-block__title"><?php echo $visit['heading']; ?></h2>
 
 			<?php
-			if ( get_sub_field('desc') ) :
-				the_sub_field('desc');
-			endif;
+			echo $visit['desc'];
 
-			if ( have_rows('ctas') ) :
-				while ( have_rows('ctas') ) : the_row();
-					$link = get_sub_field('link');
+			$ctas = $visit['ctas'];
+
+			if ( $ctas ) :
+				foreach( $ctas as $cta ) :
+					$link = $cta['link'];
 			?>
 
 			<p class="location__cta">
-				<a class="location__button" href="<?php echo esc_url( $link['url'] ); ?>"><?php echo esc_attr( $link['title'] ); ?></a>
+				<a class="location__button" href="<?php echo $link['url']; ?>"><?php echo $link['title']; ?></a>
 			</p>
 
 			<?php
-				endwhile;
+				endforeach;
 			endif;
 			?>
 
@@ -36,7 +33,3 @@ if ( have_rows( 'visit_foco', 'option' ) ) :
 	</div><!-- .location__inner -->
 
 </div><!-- .location -->
-
-<?php
-	endwhile;
-endif;
