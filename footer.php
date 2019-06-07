d176aa816ef870ebd5334c3926cdaaefff8f0536
codepixelstudio <codepixel@codepixelstudio.com>
2019-06-07 10:06:12 -0600

    make BrowserSync environmentally-dependent

--- footer.php
+++ footer.php
@@ -3,7 +3,19 @@
 
         <?php wp_footer(); ?>
 
-        <script async src="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/js/library/velocity.ui.min.js"></script>
+        <script src="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/js/library/velocity.ui.min.js"></script>
+
+        <?php
+
+            $environment = $_SERVER[ 'DOCUMENT_ROOT' ];
+
+            if ( $environment === '/var/www/html' ) {
+
+                get_template_part( 'elements/developer/browser.sync' );
+
+            }
+
+        ?>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138956035-1"></script>
