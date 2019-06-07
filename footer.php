f17d7c4741640603ae99cad1fcdd0b9fd9e43c93
codepixelstudio <codepixel@codepixelstudio.com>
2019-06-07 08:58:28 -0600

    remove BrowserSync from footer.php

--- footer.php
+++ footer.php
@@ -3,14 +3,7 @@
 
         <?php wp_footer(); ?>
 
-        <script src="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/js/library/velocity.ui.min.js"></script>
-        <script id="__bs_script__">
-
-            //<![CDATA[
-                document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
-            //]]>
-
-        </script>
+        <script async src="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/js/library/velocity.ui.min.js"></script>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138956035-1"></script>
