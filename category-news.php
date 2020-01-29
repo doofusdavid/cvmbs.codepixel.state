3e9025acb13c5e84ca9c5a42c611849aa36befc7
codepixelstudio <codepixel@codepixelstudio.com>
2020-01-29 09:43:28 -0700

    [labs] add conditional pagination display
    only render pagination controls if number of posts on archive page exceeds posts_per_page setting

--- category-news.php
+++ category-news.php
@@ -76,26 +76,18 @@
 
 		<?php endwhile; ?>
 
-		<?php $pagination = array(
-			'base'               => '%_%',
-			'format'             => '?paged=%#%',
-			'total'              => 1,
-			'current'            => 0,
-			'show_all'           => false,
-			'end_size'           => 1,
-			'mid_size'           => 2,
-			'prev_next'          => true,
-			'prev_text'          => __('« Previous'),
-			'next_text'          => __('Next »'),
-			'type'               => 'plain',
-			'add_args'           => false,
-			'add_fragment'       => '',
-			'before_page_number' => '',
-			'after_page_number'  => ''
-		); ?>
+		<?php $category = get_queried_object(); ?>
+
+		<?php $count = $category->count; ?>
+
+		<?php $posts_per_page = get_option( 'posts_per_page' ); ?>
+
+		<?php if ( $count > $posts_per_page ) : ?>
 
 		<!-- pagination -->
 		<div class="pagination-container">
+
+			<?php echo $count . ' : ' . $posts_per_page; ?>
 
 			<!-- pagination controls -->
 			<div class="pagination-controls">
@@ -107,6 +99,8 @@
 
 		</div>
 		<!-- END pagination -->
+
+		<?php endif; ?>
 
 		<?php endif; ?>
 
