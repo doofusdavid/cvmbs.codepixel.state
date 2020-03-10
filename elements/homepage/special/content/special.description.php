524b6e85d28f2faa51090d3b61ebcc9818e1eb26
codepixelstudio <codepixel@codepixelstudio.com>
2020-03-10 10:06:09 -0600

    [special unit] add PRSE option to homepage description block

--- elements/homepage/special/content/special.description.php
+++ elements/homepage/special/content/special.description.php
@@ -1,3 +1,19 @@
+<?php
+
+    // PRSE option
+    $prse_option = get_sub_field( 'prse_option' );
+
+    if ( $prse_option ) {
+
+        $desc_class = 'prse';
+
+    } else {
+
+        $desc_class = 'standard';
+
+    }
+
+?>
 
 <!-- description -->
 <section class="special-description">
@@ -19,9 +35,49 @@
         <?php endif; ?>
 
     <!-- text -->
-    <div class="description-text">
+    <div class="description <?php echo $desc_class; ?>">
 
-        <?php the_sub_field( 'content' ); ?>
+        <!-- unit description -->
+        <div class="unit_description">
+
+            <?php the_sub_field( 'content' ); ?>
+
+        </div>
+        <!-- END unit description -->
+
+        <?php if ( $prse_option ) : ?>
+
+        <!-- prse description -->
+        <div class="prse_description">
+
+            <!-- title -->
+            <span class="title">
+
+                program of research and scholarly excellence
+
+            </span>
+            <!-- END title -->
+
+            <!-- text -->
+            <span class="text">
+
+                Programs are awarded this designation because they have achieved great distinction and set a standard for excellence in research, teaching and service that may serve as a model for programs throughout CSU and externally.
+
+            </span>
+            <!-- END text -->
+
+            <!-- link -->
+            <a href="https://www.research.colostate.edu/prse/">
+
+                learn more about PRSE
+
+            </a>
+            <!-- END link -->
+
+        </div>
+        <!-- END prse description -->
+
+        <?php endif; ?>
 
     </div>
     <!-- END text -->
